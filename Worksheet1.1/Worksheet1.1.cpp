b040545b088abae43955ab824ab43433b4acb519
James Huxtable <james.huxtable@codeape.co.uk>
2017-10-09 09:33:11 +0100

    starter code added

---
+++ Worksheet1.1/Worksheet1.1.cpp
@@ -0,0 +1,14 @@
+// Worksheet1.1.cpp : Defines the entry point for the console application.
+// Number Guessing Game
+
+#include "stdafx.h"
+
+int main()
+{
+	// here your game logic should reside
+	// it may help to write your game in pseudocode first
+	// as it's very simple, it won't take many lines
+
+    return 0;
+}
+
---
+++ Worksheet1.2/Worksheet1.2.cpp
@@ -0,0 +1,11 @@
+// Worksheet1.2.cpp : Defines the entry point for the console application.
+// Here goes the cube calculation program
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
