d26b1b92c29db6ad2c392c71cde3c93598d8cd9b
BradKing17 <king.brad1997@gmail.com>
2017-10-13 11:44:02 +0100

    Task 1.1 uploaded

--- Worksheet1.1/Worksheet1.1.cpp
+++ Worksheet1.1/Worksheet1.1.cpp
@@ -2,13 +2,67 @@
 // Number Guessing Game
 
 #include "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+#define RAND_MAX 10
+
 
 int main()
-{
-	// here your game logic should reside
-	// it may help to write your game in pseudocode first
-	// as it's very simple, it won't take many lines
 
-    return 0;
+{
+
+	int guess = 0;
+
+	int end;
+
+	char playAgain = 'Y';
+
+	while (playAgain == 'Y' || playAgain == 'y')
+
+	{
+
+
+		std::srand(std::time(0)); //use current time as seed for random generator
+
+		int generatedNum = std::rand() % 10 + 1;
+
+		std::cout << "Random value on [0 " << RAND_MAX << "]: \n ";
+
+
+		std::cout << "Enter guess: ";
+
+		std::cin >> guess;
+
+
+		if (guess == generatedNum)
+
+		{
+
+			std::cout << "Congrats you win \n\n";
+
+			std::cout << "Do you want to play again (Y/N)? ";
+
+			std::cin >> playAgain;
+
+		}
+
+		else
+
+		{
+
+			std::cout << "The value was " << generatedNum << "\n\n";
+
+			std::cout << "Do you want to play again (Y/N)? ";
+
+			std::cin >> playAgain;
+
+		}
+
+	}
+
+	return 0;
+
 }
 
